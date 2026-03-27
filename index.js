495297dbdb46020deea4bf6aee4a1fd4dd5a157f
Akki786461 <khanSahil111Unlocked@gmail.com>
2026-03-27 12:34:51 +0530

    Update group thread ID and name in index.js

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ try {
 }
 
 // ✅ Group Info (change these)
-const GROUP_THREAD_ID = "1514671402956626";
-const LOCKED_GROUP_NAME = "🫸𝗦𝗜𝗧𝗔 𝗣𝗔𝗥𝗪𝗔𝗧𝗜 𝗞𝗔𝗬𝗔𝗠𝗔𝗧 𝗧𝗔𝗞 𝗖𝗛𝗨𝗗𝗘𝗚𝗜 𝗟𝗔𝗚𝗔𝗟𝗘 𝗗𝗨𝗠 𝗟𝗨𝗡𝗗𝗕𝗛𝗔𝗞𝗧 🔵💪🤣"; 
+const GROUP_THREAD_ID = "784672563950092";
+const LOCKED_GROUP_NAME = "🫸𝗞𝗨𝗠𝗔𝗥 𝗞𝗜 𝗦𝗜𝗧𝗔 𝗣𝗔𝗥𝗪𝗔𝗧𝗜 𝗞𝗔𝗬𝗔𝗠𝗔𝗧 𝗧𝗔𝗞 𝗖𝗛𝗨𝗗𝗘𝗚𝗜 𝗟𝗔𝗚𝗔𝗟𝗘 𝗗𝗨𝗠 𝗟𝗨𝗡𝗗𝗕𝗛𝗔𝗞𝗧 🔵💪🤣"; 
   // Locked nameLockedxpress Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
 const PORT = process.env.PORT || 3000;
